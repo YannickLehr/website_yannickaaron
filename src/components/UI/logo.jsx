ca6dfe0ab462c4b4505cc3196046e5ddf33ad71b
Yannick Aaron Lehr <yannick@lehr-web.de>
2022-06-14 11:37:56 +0200

    updated logo

--- src/components/UI/logo.jsx
+++ src/components/UI/logo.jsx
@@ -1,12 +1,19 @@
 import { chakra } from "@chakra-ui/react";
 import NextLink from "next/link";
+import NextImage from "next/image";
 
 const Logo = () => {
   return (
     <NextLink href="/" passHref>
-      <chakra.a fontSize="2rem" fontWeight="700">
-        LOGO
-      </chakra.a>
+        <NextImage
+          src="/logo_test.svg"
+          width="180"
+          height="350"
+          alt="avatar bigheads"
+          placeholder="blur"
+          blurDataURL="L5I~of#i004mgjw]-4XA00?wL#xu"
+          priority
+        />
     </NextLink>
   );
 };
